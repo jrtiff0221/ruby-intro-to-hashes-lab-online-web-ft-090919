bebcb55fd8a19b2a49595a1a0a3a412fd033e547
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-16 19:52:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
 	      :pennsylvania_railroad => {"mortgage_value" => "$200"},
 	      :b_and_o_railroad => {"mortgage_value" => "$400"},
 	      :shortline => {"mortgage_value" => "$800"}
-	      
+	    },
 	     :rent_in_dollars => { 
 	       :one_piece_owned => 25,
          :two_pieces_owned => 50,
@@ -47,7 +47,6 @@ def monopoly_with_third_tier
 	       }
 	      }
 	    }
-	  } 
   end
 
 def monopoly_with_fourth_tier
