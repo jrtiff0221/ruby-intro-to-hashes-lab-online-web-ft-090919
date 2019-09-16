110775ed74fb1306aa1b23baa62502578ccb453f
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-16 19:03:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,12 @@ def monopoly_with_second_tier
 		monopoly = {
 	  :railroads => { 
 	    :pieces => 4
-	    :rent_in_dollars => {}
-	     :name => {}
+	    :rent_in_dollars => {
+	      
+	    }
+	     :name => {
+	       
+	     }
 	   
 	  }
 	}
